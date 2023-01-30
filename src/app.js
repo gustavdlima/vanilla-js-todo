2251e98e168e52b2824c7a0af5378db6e9b2e9f3
gstv <gustavdlim@gmail.com>
2023-01-30 11:12:00 -0300

    feat: when you click the checkbox in task item, now prints a message

--- src/app.js
+++ src/app.js
@@ -10,7 +10,20 @@ function newTask(input) {
 
 	todoItems.push(task);
 	renderTask(task);
-	console.log(todoItems);
+};
+
+function toggleDone(key) {
+	const	list = document.querySelector('.todo-list');
+	const 	index = todoItems.findIndex(item => item.id === Number(key));
+
+	todoItems[index].checked = !todoItems[index].checked;
+	const msg = document.createElement("div");
+	msg.innerText = "wtf.. you finished ";
+	list.append(msg);
+
+	const item = document.querySelector(`[data-key='${todoItems[index].id}']
+	`);
+	item.remove();
 }
 
 function renderTask(task) {
@@ -21,23 +34,34 @@ function renderTask(task) {
 	node.setAttribute('class', 'todo-item ${isChecked}');
 	node.setAttribute('data-key', task.id);
 
-	// <input id="${task.id}" type="checkbox"/>
 	node.innerHTML = `
-	<label for="${task.id}" class="tick js-tick"></label>
-	<button class="delete-todo js-delete-todo">
-	<svg><use href="#delete-icon"></use></svg>
-	</button>
+		<input id="${task.id}" type="checkbox"/>
+		<label for="${task.id}" class="task-tag"></label>
+		<span>${task.input}</span>
+		<button class="delete-todo">x
+		</button>
 	`;
 
 	list.append(node);
-}
+};
 
 const form = document.querySelector(".todo-form");
-
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   var input = document.querySelector(".todo-input").value;
 
   newTask(input);
+});
+
+const list = document.querySelector('.todo-list');
+list.addEventListener('click', event => {
+	if (!event.target.classList.contains('task-tag')) {
+		const itemKey = event.target.parentElement.dataset.key;
+		toggleDone(itemKey);
+	}
+	else if (!event.target.classList.contains('delete-todo')) {
+		const itemKey = event.target.parentElement.dataset.key;
+		toggleDone(itemKey);
+	}
 });
